1a74045c0a9fc8114d9e91443079c1053097042f
fraserbrown87 <fraserbrown87@gmail.com>
2018-02-04 21:31:27 +0000

    second commit

--- public/app.js
+++ public/app.js
@@ -5,12 +5,38 @@ var makeRequest = function(url, callback){
   request.send();
 }
 
-// var getImage = function(character){
-//   var img = document.createElement('img');
-//   img.src = character.image;
-//   img.width = "200";
-//   return img;
-// }
+var handleButtonClick = function () {
+  var name = getInputValue('name');
+  var house = getInputValue('house');
+  var actor = getInputValue('actor')
+
+  var character = {
+    name: name,
+    house: house,
+    actor: actor
+  };
+
+  setDefinitionText('name-display', character.name);
+  setDefinitionText('house-display', character.house);
+  setDefinitionText('actor-display', character.actor);
+
+  localStorage.setItem('character', character);
+}
+
+var formDropdown = function(characters){
+  var select = document.querySelector('#character-dropdown')
+  var optionAtTop = document.createElement('option');
+  optionAtTop.innerText = "Select a character";
+  optionAtTop.disabled = true;
+  optionAtTop.selected = true;
+  select.appendChild(optionAtTop);
+  characters.forEach(function(character, index){
+    var option = document.createElement('option');
+    option.innerText = character.name;
+    option.value = index;
+    select.appendChild(option);
+  })
+}
 
 var populateList = function(characters){
   var ul = document.querySelector('#character-list');
@@ -32,25 +58,6 @@ var populateList = function(characters){
   });
 }
 
-// var populatelist = function(characters){
-//   var ul = document.querySelector('#character-list');
-//
-//   characters.forEach(function(character){
-//     var li = document.createElement('li');
-//     var name = document.createElement('p');
-//     title.innerText = character.name;
-//     var house = document.createElement('p');
-//     year.innerText = character.house;
-//     var img = document.createElement('img');
-//     img.src = character.image;
-//     img.width = 200;
-//     // console.log(movie.Title);
-//     li.appendChild(name);
-//     li.appendChild(house);
-//     li.appendChild(img);
-//     ul.appendChild(li);
-//   })
-// }
 
 
 var requestComplete = function(){
@@ -59,16 +66,33 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var characters = JSON.parse(jsonString); //NEW
   populateList(characters);
+  populateDropdown(characters);
+  formDropdown(characters);
+
 }
+
+// var jsonString = JSON.stringify(character) // NEW
+// localStorage.setItem('character', jsonString);
 
 
 var app = function(){
   var url = 'http://hp-api.herokuapp.com/api/characters';
   makeRequest(url, requestComplete);
 
-  var select = document.querySelector('select');
-  select.addEventListener('change', handleSelectChange)
+  // populateDropdown();
 
+  var select = document.querySelector('#character-dropdown');
+  select.addEventListener('change', handleOptionSelected);
+
+  var button = document.querySelector('button');
+  button.addEventListener('click', handleButtonClick);
+
+  // var jsonString = localStorage.getItem('character');
+  // var savedCharacter = JSON.parse(jsonString);
+  //
+  // setDefinitionText('#name-display', savedCharacter.name);
+  // setDefinitionText('#house-display', savedCharacter.house);
+  // setDefinitionText('#actor-display', savedCharacter.actor);
 }
 
 window.addEventListener('load', app)
